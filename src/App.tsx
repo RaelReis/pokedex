c5580617d5fae6a441aa44f0ef1d31670481cb86
RaelReis <rafael.reeis03@gmail.com>
2022-07-30 18:16:34 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./app.css";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { pokemonService } from "./services/pokemonService";
 import { Pokemon } from "./interfaces/pokemon";
 
@@ -41,14 +41,14 @@ function App() {
   }, [search]);
 
   const handleNextClick = async () => {
-    setPokemonLoading(true)
+    loadedPokemon && setPokemonLoading(true)
     const newPokemon = await pokemonService.nextPokemon(pokemon);
     newPokemon && setPokemon(newPokemon);
     setSearch('')
   };
 
   const handlePreviousClick = async () => {
-    setPokemonLoading(true)
+    loadedPokemon && setPokemonLoading(true)
     const newPokemon = await pokemonService.previousPokemon(pokemon);
     newPokemon && setPokemon(newPokemon);
     setSearch('')
@@ -95,16 +95,15 @@ function App() {
     <div className="App">
       <main className="pokedex">
         {/* Pokedex */}
-        <img src={pokedex} alt="Imagem de um Pokedex" />
+        <img className="pokedex__image" src={pokedex} alt="Imagem de uma Pokedex" />
 
         {/* Pokemon Info */}
-        <div className="pokemon-box">
+        <div className="pokedex__box">
 
           {pokemon && randerPokemonImage()}
 
-          <div className={`searchingLamp ${pokemonLoading ? 'active': ''}`}/>
-          <div className="searchingLampBackground"/>
-          <div className="pokemon-info">
+          <div className={`pokedex__box__searching-lamp ${pokemonLoading ? 'active': ''}`}/>
+          <div className="pokedex__box__pokemon-info">
 
             {loadedPokemon 
               ? renderPokemonInfo()
@@ -112,7 +111,7 @@ function App() {
 
           </div>
         </div>
-        <div className="input-wrapper">
+        <div className="pokedex__input-wrapper">
           <input
             type="text"
             placeholder="Digite um pokemon"
@@ -121,12 +120,16 @@ function App() {
             ref={searchInput}
           />
         </div>
-        <div className="buttons-wrapper">
+        <div className="pokedex__buttons-wrapper">
           <button onClick={handlePreviousClick}><img src={leftIcon} alt="" /></button>
           <button onClick={handleNextClick}><img src={rightIcon} alt="" /></button>
         </div>
       </main>
+      <footer className="footer">
+        <span className="footer__text">Created By <a className="footer__link" href="https://github.com/RaelReis" target="_blank">Rafael Reis</a></span>
+      </footer>
     </div>
+  
   );
 }
 
--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,14 +1,11 @@
 import { Pokemon } from "../interfaces/pokemon";
 
 const API_URL = "https://pokeapi.co/api/v2/pokemon/";
-const fetchConfig = {
-  method: "GET",
-};
 
 export const pokemonService = {
   async searchPokemon(pokemonName: string, currentPokemon: Pokemon | null): Promise<Pokemon | null> {
     if (pokemonName.length > 0) {
-      const res = await fetch(API_URL + pokemonName, fetchConfig);
+      const res = await fetch(API_URL + pokemonName);
       if (res.status === 200) {
         const data = await res.json();
         return data;
